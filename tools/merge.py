822527f1c745ef33a6094cc6dab62833b2dfa47c
Zhall <rzlamrr.dvst@protonmail.com>
2021-07-13 06:26:38 +0000

    tools: merge: replace url value

--- tools/merge.py
+++ tools/merge.py
@@ -5,5 +5,7 @@ with open("website_api.json", "w") as outfile:
     for i in glob.glob("builds/*.json"):
         with open(i, 'rb') as infile:
             data = json.load(infile)
+            for item in data:
+                item['url'] = "https://www.pling.com/p/1544683"
             head += data
     json.dump(head, outfile)
