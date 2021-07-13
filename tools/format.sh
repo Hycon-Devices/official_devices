36245d7b5cb9949d89595122c32228c0175292a2
Zhall <rzlamrr.dvst@protonmail.com>
2021-07-13 02:57:33 +0000

    import json formatter

---
+++ tools/format.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git config --global user.email "rzlamrr.dvst@protonmail.com"
+git config --global user.name "rzlamrr"
+
+rm -f builds/website_api.json
+python tools/merge.py
+mv website_api.json builds/website_api.json
+
+for i in $(find . -type f -iname '*.json')
+do
+    mkdir tmp
+    python -m json.tool < $i > tmp/format.json
+    mv tmp/format.json $i
+    rm -rf tmp
+done
+
+git commit -am "json: autoformat"
