c2b971b55d4853b60b17f906b2763d6fdb960783
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-07-15 08:44:36 +0700

    tools: prepare dropped devices

--- tools/merge.py
+++ tools/merge.py
@@ -2,6 +2,7 @@ import glob
 import json
 
 head = []
+dropped = ["wayne"]
 outfile = open("website_api.json", 'w')
 
 for x in glob.glob("builds/*.json"):
@@ -9,6 +10,9 @@ for x in glob.glob("builds/*.json"):
         data = json.load(file)
         for item in data:
             item['url'] = "https://www.pling.com/p/1544683"
+            for d in dropped:
+                if d in item["filename"]:
+                    item["version"] = item["version"] + ", Dropped"
         head += data
 
 json.dump(head, outfile)
