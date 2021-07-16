513ea6ffd4eed84b8f7a3534feab511135945fa5
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-07-16 22:48:17 +0000

    tools: fixed not exitting when json is broken

--- tools/validate.py
+++ tools/validate.py
@@ -1,8 +1,20 @@
 import glob
 import json
+import os
+
+os.system("rm -f brokenjson.txt")
 
 for x in glob.glob("**/**json", recursive=True):
     try:
         data = json.loads(open(x).read())
     except Exception as e:
-        print(f"{x}: {e}")
+        with open('brokenjson.txt', 'a') as file:
+            file.write(f'{x}: {e}\n')
+
+if os.path.isfile("brokenjson.txt"):
+    with open('brokenjson.txt', 'r+') as file:
+        lines = file.readlines()
+        for line in lines:
+            print(line)
+    os.system("rm -f brokenjson.txt")
+    exit(1)
