9eb76a6acc287c03d1ae513052e0bfd79a9cf561
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-07-17 16:51:09 +0700

    tools: prepare dropped device bring back wayne stuff

--- tools/merge.py
+++ tools/merge.py
@@ -3,6 +3,7 @@ import json
 import os
 
 head = []
+dropped = ["wayne", "ysl"]
 
 os.system("rm -f builds/website_api.json")
 
@@ -11,6 +12,9 @@ for x in glob.glob("builds/*.json"):
         data = json.load(file)
         for item in data:
             item['url'] = "https://www.pling.com/p/1544683"
+            for d in dropped:
+                if d in item["filename"]:
+                    item["version"] = item["version"] + ", Dropped"
         head += data
 
 with open("builds/website_api.json", 'w') as outfile:
