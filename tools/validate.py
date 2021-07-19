d2192b4ad65cdf90391d530f7a5c6260591c98d7
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-07-19 03:00:09 +0000

    tools, workflows: improve and send msg to telegram
    inspired by https://github.com/Evolution-X-Devices/official_devices/blob/master/runner.sh

--- tools/validate.py
+++ tools/validate.py
@@ -8,13 +8,9 @@ for x in glob.glob("**/**json", recursive=True):
     try:
         data = json.loads(open(x).read())
     except Exception as e:
+        print(f'{x}: {e}\n')
         with open('brokenjson.txt', 'a') as file:
-            file.write(f'{x}: {e}\n')
+            file.write(f'- {x}: {e}\n')
 
 if os.path.isfile("brokenjson.txt"):
-    with open('brokenjson.txt', 'r+') as file:
-        lines = file.readlines()
-        for line in lines:
-            print(line)
-    os.system("rm -f brokenjson.txt")
     exit(1)
